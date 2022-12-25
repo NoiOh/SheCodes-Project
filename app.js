3183078e8dd4c9538dbfac731f35ea4150486424
Noi Houangvan <45153151+NoiOh@users.noreply.github.com>
2022-12-24 21:22:41 -0500

    added unit conversion

--- app.js
+++ app.js
@@ -30,12 +30,14 @@ function showWeather(response) {
   let country = response.data.sys.country;
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+
+  celsuisTemperature = response.data.main.temp;
+
   document.querySelector("#city-name").innerHTML = `${city}, ${country}`;
   document.querySelector("#forecast-description").innerHTML =
     response.data.weather[0].description;
-  document.querySelector("#forecast-temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  document.querySelector("#forecast-temp").innerHTML =
+    Math.round(celsuisTemperature);
   document.querySelector("#humidity").innerHTML = `${Math.round(
     response.data.main.humidity
   )}%`;
@@ -65,9 +67,6 @@ function searchCity(event) {
   search(city);
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchCity);
-
 // Current Location
 function retrievePosition(position) {
   //console.log(position);
@@ -84,7 +83,38 @@ function getCurrentPosition(event) {
   navigator.geolocation.getCurrentPosition(retrievePosition);
 }
 
+// unit conversion
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#forecast-temp");
+  //css
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
+  let fahrenheitTemperature = (celsuisTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsuis(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#forecast-temp");
+  //css
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+  temperatureElement.innerHTML = Math.round(celsuisTemperature);
+}
+
+let celsuisTemperature = null;
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+
 let currentButton = document.querySelector("#current-location");
 currentButton.addEventListener("click", getCurrentPosition);
+
+let fahrenheit = document.querySelector("#fahrenheit-link");
+fahrenheit.addEventListener("click", displayFahrenheit);
+
+let celsius = document.querySelector("#celsius-link");
+celsius.addEventListener("click", displayCelsuis);
 
 search("New York");
